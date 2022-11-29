bb836592a75bc4147fa5facdc6d3ef0fab7d2637
Tate Weaver <weave2ta@dukes.jmu.edu>
2022-11-29 10:43:01 -0500

    working caluclator API

--- js/main.js
+++ js/main.js
@@ -1,24 +1,30 @@
 
-const element = document.getElementById("butt");
-element.addEventListener("click", getLove);
+
 
 
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+		'X-RapidAPI-Key': '6de12a74a5mshe9e3ca19c9b329dp1b5d75jsnfb6a3f4ebfd6',
 		'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com'
 	}
 };
 
 function getLove() {
+    
     const sname = document.getElementById('query1');
     const fname = document.getElementById('query2');
+    console.log(sname.value)
+    console.log(fname.value)
+
+    fetch(`https://love-calculator.p.rapidapi.com/getPercentage?sname=${sname.value}&fname=${fname.value}`, options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+
 
 }
-
-
-
 
 
 // const options = {
